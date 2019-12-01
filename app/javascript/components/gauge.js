8761c1097eab407ba132f721b194126f2c0a279d
Lionel Melin <lionel.melin@gmail.com>
2019-12-01 12:46:36 +0100

    js gauge half way... and modal profile half way

--- app/javascript/components/gauge.js
+++ app/javascript/components/gauge.js
@@ -3,6 +3,7 @@ const initGauge = () => {
 
 $(function() {
 
+
   $(".progress").each(function() {
 
     var value = $(this).attr('data-value');
@@ -11,55 +12,54 @@ $(function() {
 
     if (value >= 0) {
       if (value <= 50) {
-        // right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)')
-        right.animate([
-          // keyframes
-          { transform: 'rotate(0deg)'},
-          { transform: 'rotate(' + percentageToDegrees(value) + 'deg)' }
-        ], {
-          // timing options
-          duration: 3000,
-          fill: "forwards",
-          easing: "linear"
-        });
-        console.log("hello world <50");
+        right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)')
+        // right.animate([
+        //   // keyframes
+        //   { transform: 'rotate(0deg)'},
+        //   { transform: 'rotate(' + percentageToDegrees(value) + 'deg)' }
+        // ], {
+        //   // timing options
+        //   delay: 0,
+        //   direction: "normal",
+        //   duration: 300,
+        //   fill: "forwards",
+        //   easing: "linear"
+        // });
+        // console.log(percentageToDegrees(value));
       } else {
-        // right.css('transform', 'rotate(180deg)')
-        // left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)')
-        right.animate([
-          // keyframes
-          { transform: 'rotate(0deg)' },
-          { transform: 'rotate(180deg)'}
-        ], {
-          // timing options
-          delay: 0,
-          direction: "normal",
-          duration: 3000,
-          fill: "forwards",
-          easing: "linear"
-        });
-  console.log("hello world >50");
-        left.animate([
-          // keyframes
-          { transform: 'rotate(0deg)' },
-          { transform: 'rotate(' + percentageToDegrees(value - 50) + 'deg)'}
-        ], {
-          // timing options
-          delay: 0,
-          direction: "normal",
-          duration: 3000,
-          fill: "forwards",
-          easing: "linear"
-        });
+        right.css('transform', 'rotate(180deg)')
+        // right.animate([
+        //   // keyframes
+        //   { transform: 'rotate(0deg)' },
+        //   { transform: 'rotate(180deg)'}
+        // ], {
+        //   // timing options
+        //   delay: 0,
+        //   direction: "normal",
+        //   duration: 300,
+        //   fill: "forwards",
+        //   easing: "linear"
+        // });
+        // console.log(percentageToDegrees(value - 50));
+        left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)')
+        // left.animate([
+        //   // keyframes
+        //   { transform: 'rotate(0deg)' },
+        //   { transform: 'rotate(' + percentageToDegrees(value - 50) + 'deg)'}
+        // ], {
+        //   // timing options
+        //   delay: 0,
+        //   direction: "normal",
+        //   duration: 300,
+        //   fill: "forwards",
+        //   easing: "linear"
+        // });
       }
     }
-
   })
 
   function percentageToDegrees(percentage) {
-
     return percentage / 100 * 360
-
   }
 
 });
