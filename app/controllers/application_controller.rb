cc0966a168af8f607094af974ea07edce4547e02
Lionel Melin <lionel.melin@gmail.com>
2019-12-05 13:17:28 +0100

    application controller fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
   include Pundit
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
@@ -10,8 +14,4 @@ class ApplicationController < ActionController::Base
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
-end
-
-def default_url_options
-  { host: ENV["DOMAIN"] || "localhost:3000" }
 end
